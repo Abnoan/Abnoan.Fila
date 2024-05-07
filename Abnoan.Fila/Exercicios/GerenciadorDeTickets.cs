35dbb511374f704fffe3c4cb9c35728debbebe08
Abnoan Muniz <abnoanmuniz@gmail.com>
2024-05-07 16:56:31 -0300

    Ajustes

--- Abnoan.Fila/Exercicios/GerenciadorDeTickets.cs
+++ Abnoan.Fila/Exercicios/GerenciadorDeTickets.cs
@@ -58,8 +58,53 @@ namespace Abnoan.Fila.Exercicios
         }
 
         #region Não Usado
-        private void Utilidade(){
-            
+        private void Utilidade()
+        {
+            // Criar uma simulação de sistema de atendimento ao cliente onde os clientes são atendidos na ordem de chegada.
+            Queue<string> filaAtendimento = new Queue<string>();
+            filaAtendimento.Enqueue("Ana");
+            filaAtendimento.Enqueue("João");
+            Console.WriteLine(filaAtendimento.Dequeue() + " foi atendido.");
+
+            // Implementar um sistema de gerenciamento de tarefas onde as tarefas devem ser executadas em uma sequência específica.
+
+            Queue<string> filaTarefas = new Queue<string>();
+            filaTarefas.Enqueue("Enviar email");
+            filaTarefas.Enqueue("Preparar relatório");
+            Console.WriteLine(filaTarefas.Dequeue() + " foi realizada.");
+
+            // Desenvolva uma simulação de fila de atendimento em um banco, 
+            // onde os clientes têm prioridades diferentes e podem chegar em momentos distintos.
+
+            Queue<string> filaNormal = new Queue<string>();
+            Queue<string> filaPrioritaria = new Queue<string>();
+            filaPrioritaria.Enqueue("João");
+            filaNormal.Enqueue("Ana");
+            filaNormal.Enqueue("Luís");
+
+            while (filaPrioritaria.Count > 0)
+            {
+                Console.WriteLine(filaPrioritaria.Dequeue() + " foi atendido.");
+            }
+            while (filaNormal.Count > 0)
+            {
+                Console.WriteLine(filaNormal.Dequeue() + " foi atendido.");
+            }
+
+            //Implementar um sistema de controle de processos de produção 
+            //onde as máquinas executam tarefas em uma ordem específica, 
+            //e novas tarefas são adicionadas dinamicamente.
+
+            Queue<string> filaProducao = new Queue<string>();
+            filaProducao.Enqueue("Cortar material");
+            filaProducao.Enqueue("Montar peça");
+            filaProducao.Enqueue("Inspeção de qualidade");
+
+            Console.WriteLine("Próxima tarefa: " + filaProducao.Peek());
+            while (filaProducao.Count > 0)
+            {
+                Console.WriteLine(filaProducao.Dequeue() + " completada.");
+            }
         }
         #endregion
     }
